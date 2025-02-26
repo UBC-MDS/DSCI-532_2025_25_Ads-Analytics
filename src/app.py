99faa07f382f6ed24afb83b41491cccd5170c40a
dd5124 <yeji.sohn.0601@gmail.com>
2025-02-26 15:22:55 -0800

    app.run

--- src/app.py
+++ src/app.py
@@ -11,5 +11,5 @@ app.layout = html.Div('I am alive')
 # ...
 
 # Run the app/dashboard
-if __name__ == '__main__':
-    app.server.run(port= 8001, host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(debug=True)
