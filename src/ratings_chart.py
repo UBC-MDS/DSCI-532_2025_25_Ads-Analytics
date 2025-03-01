632acbd80fbb60adc3fc76d5f61f29afb2a38611
Quanhua Huang <quanhua@student.ubc.ca>
2025-03-01 11:38:48 -0800

    update axis in average ratings plot

--- src/ratings_chart.py
+++ src/ratings_chart.py
@@ -7,8 +7,6 @@ def ratings_chart(df):
 
     Parameters:
         df (pd.DataFrame): The DataFrame containing app data.
-        min_rating (float): Minimum rating to filter apps.
-        max_rating (float): Maximum rating to filter apps.
 
     Returns:
         object of the Altair chart
@@ -16,11 +14,14 @@ def ratings_chart(df):
 
     #df = df.query("Rating >= @min_rating and Rating <= @max_rating")
     average_ratings = df.groupby('Category')['Rating'].mean().reset_index()
-    
+    #average_ratings['Rating'] = pd.to_numeric(average_ratings['Rating'], errors='coerce')
+    min_rating = average_ratings['Rating'].min()
+    max_rating = average_ratings['Rating'].max()
 
     chart = alt.Chart(average_ratings).mark_bar().encode(
         y=alt.Y("Category:N", sort="-x", title="App Category"),  
-        x=alt.X("Rating:Q", title="Average Ratings", axis=alt.Axis(format=",.0f")), 
+        x=alt.X("Rating:Q", title="Average Ratings", axis=alt.Axis(format=",.1f", tickCount=6)),
+         
         color=alt.Color("Category:N", legend=None),
         tooltip=['Category', 'Rating']
     ).properties(
