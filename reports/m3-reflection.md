61b8ac2a84a5dd0c489892fd377108d1f45d77da
dd5124 <72294180+dd5124@users.noreply.github.com>
2025-03-09 08:13:01 -0700

    Changed Top 10 App by Install plot to Wordcloud (#56)
    * add wordcloud requirement
    
    * create wordcloud
    
    * fix alignemnt issue
    
    * add reflection

--- reports/m3-reflection.md
+++ reports/m3-reflection.md
@@ -2,8 +2,12 @@
 
 ## 1. Implemented Parts Since Milestone 2
 - Added missing demo.gif in README.md
+- Splited the large app.py into 3 parts (app.py, layout.py, callbacks.py). Also organized src folder better by moving all charts creation related scripts to charts/
+- Changed the bottom bar chart (Top 10 apps by installs) to word cloud
 
 ## 2. Parts Not Yet Implemented
+- Performance improvement
+    - This will be implemented in Milestone 4, as this will be covered in next week's lecture.
 
 ## 3. Differences from Proposal/Sketch
 
--- src/charts/ranking_chart.py
+++ src/charts/ranking_chart.py
@@ -1,38 +1,45 @@
-import altair as alt
 import pandas as pd
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+from dash import dcc
+import plotly.graph_objects as go
 
-def ranking_chart(df, selected_type="Free", min_rating=4):
+def create_wordcloud(df, categories):
     """
-    Generate an interactive Altair bar chart for the Top 10 Apps by Installs.
+    Generate a word cloud for the Top 10 App Categories by Installs.
 
     Parameters:
         df (pd.DataFrame): The DataFrame containing app data.
-        selected_type (str): "Free" or "Paid" to filter by app type.
+        selected_type (str): "Free" or "Paid".
         min_rating (float): Minimum rating to filter apps.
-
-    Returns:
-        object of the Altair chart
     """
-    filtered_df = df[(df["Type"] == selected_type) & (df["Rating"] >= min_rating)]
+    if "All" not in categories:
+        df = df[df["Category"].isin(categories)]
+        
+    top_categories = df.groupby("Category")["Installs"].sum().reset_index()
 
-    top_apps = filtered_df.groupby("App")["Installs"].sum().reset_index()
+    top_categories = top_categories.sort_values(by="Installs", ascending=False).head(10)
+    
+    # Create a dictionary for the word cloud where keys are categories and values are install counts
+    wordcloud_data = dict(zip(top_categories["Category"], top_categories["Installs"]))
+    
+    wordcloud = WordCloud(width=800, height=400, background_color="white")
+    wordcloud.generate_from_frequencies(wordcloud_data)
+    
+    # generate image
+    wordcloud_img = wordcloud.to_image()
 
-    top_apps["Installs"] = pd.to_numeric(top_apps["Installs"], errors='coerce')
-
-    top_apps = top_apps.dropna(subset=["Installs"])
-
-    top_apps = top_apps.sort_values(by="Installs", ascending=False).head(10)
-
-    chart = alt.Chart(top_apps).mark_bar().encode(
-        y=alt.Y("App:N", sort="-x", title="App"),
-        x=alt.X("Installs:Q", title="Total Installs"), 
-        color=alt.Color("App:N", legend=None),
-        tooltip=[alt.Tooltip("App:N", title="App Name"),
-                 alt.Tooltip("Installs:Q", title="Total Installs")]
-    ).properties(
-        title="Top 10 Apps by Installs",
-        width=900,
-        height=800
+    fig = go.Figure()
+    fig.add_trace(go.Image(z=wordcloud))
+    fig.update_layout(
+        height=400,
+        xaxis={"visible": False},
+        yaxis={"visible": False},
+        margin={"t": 0, "b": 0, "l": 0, "r": 0},
+        hovermode=False,
+        paper_bgcolor="#F9F9FA",
+        plot_bgcolor="#F9F9FA",
     )
-
-    return chart
+    
+    return fig
+    
