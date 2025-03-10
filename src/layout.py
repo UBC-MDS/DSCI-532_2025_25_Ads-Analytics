d0672d132b3dfe52b530e3989ff021418561d264
Ismail Bhinderwala <husmail786@gmail.com>
2025-03-09 17:24:54 -0700

    Removed redundant charts as suggested by Joel

--- src/layout.py
+++ src/layout.py
@@ -3,7 +3,6 @@ from dash import Dash, dcc, html, Input, Output, dash_table
 import dash_bootstrap_components as dbc
 import dash_vega_components as dvc
 from src.data_import import get_dropdown_options
-from src.charts.install_chart import installs_chart
 from src.charts.engagement_chart import engagement_chart
 from src.get_summary_stats import get_summary_stats
 from src.charts.make_density_plot import make_density_plot
@@ -12,141 +11,100 @@ from src.charts.ranking_chart import create_wordcloud
 from src.charts.make_popularity_score import make_popularity_score
 
 def create_global_filters(df):
-    """
-    Create the global filters for the Dash app.
-    
-    Parameters:
-    df (pd.DataFrame): The DataFrame containing the data.
-    
-    Returns:
-    list: A list of Dash components representing the global filters.
-    """
     return [
         dbc.Col([
-        html.H5('Global controls'),
-        html.Br(),
-
-        dbc.Label("Select Category:"),
-        dcc.Dropdown(
-            id="category-filter",
-            options=get_dropdown_options(df, "Category"),
-            value=["All"],
-            multi=True,
-            maxHeight=200
-        ),
-        html.Br(),
-
-        dbc.Label("Rating Range:"),
-        dcc.RangeSlider(
-            id="rating-slider",
-            min=1,
-            max=5,
-            step=0.5,
-            marks={i: str(i) for i in range(1, 6)},
-            value=[1, 5],
-            updatemode='drag'
-        ),
-        html.Br(),
-
-        dbc.Label("Select App Type:"),
-        dcc.Dropdown(id="app-type-filter", 
-                     options=get_dropdown_options(df, "Type"),
-                     value=["All"], 
-                     multi=True),
-        html.Br(),
-
-        dbc.Label("Select Content Rating:"),
-        dcc.Dropdown(
-            id="content-rating-filter",
-            options=get_dropdown_options(df, "Content Rating"),
-            value=["All"], 
-            multi=True
-        ),
-        html.Br()
+            html.H5('Global controls'),
+            html.Br(),
+            dbc.Label("Select Category:"),
+            dcc.Dropdown(
+                id="category-filter",
+                options=get_dropdown_options(df, "Category"),
+                value=["All"],
+                multi=True,
+                maxHeight=200
+            ),
+            html.Br(),
+            dbc.Label("Rating Range:"),
+            dcc.RangeSlider(
+                id="rating-slider",
+                min=1,
+                max=5,
+                step=0.5,
+                marks={i: str(i) for i in range(1, 6)},
+                value=[1, 5],
+                updatemode='drag'
+            ),
+            html.Br(),
+            dbc.Label("Select App Type:"),
+            dcc.Dropdown(
+                id="app-type-filter", 
+                options=get_dropdown_options(df, "Type"),
+                value=["All"], 
+                multi=True
+            ),
+            html.Br(),
+            dbc.Label("Select Content Rating:"),
+            dcc.Dropdown(
+                id="content-rating-filter",
+                options=get_dropdown_options(df, "Content Rating"),
+                value=["All"], 
+                multi=True
+            ),
+            html.Br()
         ],
         style={
             'background-color': '#e6e6e6',
             'padding': 10,
             'border-radius': 3,
-        }
-        ) 
+        }) 
     ]
 
 def create_layout(df):
-    """
-    Create the layout for the Dash app.
-    
-    Parameters:
-    df (pd.DataFrame): The DataFrame containing the data.
-    
-    Returns:
-    dbc.Container: The layout of the Dash app.
-    """
     global_filters = create_global_filters(df)
 
-    install_chart = dbc.Card([
-        dbc.CardHeader('Top 10 App Categories by Installs', style={'fontWeight': 'bold',
-                                                                   "textAlign": "center",}),
-        dbc.CardBody(
-            dvc.Vega(
-            id="category-chart",
-            spec=installs_chart(df).to_dict(format="vega"),
-            style={"padding": "20px", 
-                   "justifyContent": "center",  
-                   "alignItems": "center", 
-                   'width': '100%',
-                   'height': '100%' 
-                   }
-            ))
-    ])
-
     make_engagement_chart = dbc.Card([
-        dbc.CardHeader('Reviews vs. Installs for Top Apps', style={'fontWeight': 'bold',
-                                                                   "textAlign": "center",}),
+        dbc.CardHeader('Reviews vs. Installs for Top Apps', style={'fontWeight': 'bold', "textAlign": "center"}),
         dbc.CardBody(
             dvc.Vega(
-            id="engagement-chart",
-            spec=engagement_chart(df, ["All"]).to_dict(format="vega"),
-            style={"padding": "20px", 
-                   "justifyContent": "center",  
-                   "alignItems": "center",
-                   'width': '100%',
-                   'height': '100%' 
-                   }
-            ))
+                id="engagement-chart",
+                spec=engagement_chart(df, ["All"]).to_dict(format="vega"),
+                style={"padding": "20px", 'width': '100%', 'height': '100%'}
+            )
+        )
     ])
 
     density_plot = dbc.Card([
-        dbc.CardHeader('Density Plot for Ratings', style={'fontWeight': 'bold',
-                                                          "textAlign": "center",}),
+        dbc.CardHeader('Density Plot for Ratings', style={'fontWeight': 'bold', "textAlign": "center"}),
         dbc.CardBody(
             dvc.Vega(
-            id="density-plot",
-            spec=make_density_plot(df, ["All"]).to_dict(format="vega"),
-            style={"padding": "1rem 4rem 1rem"}
-        ))
+                id="density-plot",
+                spec=make_density_plot(df, ["All"]).to_dict(format="vega"),
+                style={"padding": "20px", 'width': '100%', 'height': '100%'}
+            )
+        )
     ])
 
     popularity_histogram = dbc.Card([
-        dbc.CardHeader('Average Popularity Score by Categories', style={'fontWeight': 'bold',
-                                                          "textAlign": "center",}),
+        dbc.CardHeader('Average Popularity Score by Categories', style={'fontWeight': 'bold', "textAlign": "center"}),
         dbc.CardBody(
             dvc.Vega(
-            id="popularity-histogram",
-            spec=make_popularity_score(df, ["All"]).to_dict(format="vega"),
-            style={"padding": "1rem 4rem 1rem"}
-        ))
+                id="popularity-histogram",
+                spec=make_popularity_score(df, ["All"]).to_dict(format="vega"),
+                style={"padding": "20px", 'width': '100%', 'height': '100%'}
+            )
+        )
     ])
 
     wordcloud_component = dbc.Card([
-        dbc.CardHeader('Word Cloud of Top Apps', style={'fontWeight': 'bold',
-                                                          "textAlign": "center",}),
+        dbc.CardHeader('Word Cloud of Top Apps', style={'fontWeight': 'bold', "textAlign": "center"}),
         dbc.CardBody(
             dcc.Graph(
-            id="wordcloud",
-            figure=create_wordcloud(df, ["All"]),
-            config={"displayModeBar": False}
-        ))
+                id="wordcloud",
+                figure=create_wordcloud(df, ["All"]),
+                config={"displayModeBar": False},
+                style={'width': '100%', 'height': '100%'}
+            )
+        )
     ])
 
     return dbc.Container([
@@ -190,30 +148,14 @@ def create_layout(df):
                 ], className="mt-3 mb-4", justify="center"),
 
                 dbc.Row([
-                    dbc.Col(install_chart, md=6),  
+                    dbc.Col(popularity_histogram, md=6),  # Moved to top-left
                     dbc.Col(make_engagement_chart, md=6)
                 ]),
 
                 dbc.Row([
                     dbc.Col(density_plot, md=6),
-                    dbc.Col(popularity_histogram, md=6)
-                ]),
-
-                dbc.Row([ 
-                    dbc.Col(wordcloud_component, md=12)
+                    dbc.Col(wordcloud_component, md=6)  # Moved Word Cloud here
                 ])
             ], md=10)
-        ], className="border-top pt-3"),
-
-        # Footer
-        dbc.Row(dbc.Col([
-            html.Hr(),
-            html.H6('This dashboard helps advertisement companies identify the most promising Google Play Store apps for ad placements by analyzing app metrics such as user engagement and ratings', 
-                    className="text-center fw-light mb-4",
-                    style={"maxWidth": "60%", "margin": "auto", "whiteSpace": "normal", "wordWrap": "break-word"}
-                    ),
-            html.P("Project by: Quanhua Huang, Yeji Sohn, Lukman Lateef, Ismail (Husain) Bhinderwala", className="text-center fw-light"),
-            html.P(["GitHub Repository: ", html.A("Link to Repo", href="https://github.com/UBC-MDS/DSCI-532_2025_25_Ads-Analytics", target="_blank")], className="text-center"),
-            html.P("Last Updated: March 2025", className="text-center fw-light")
-        ], width=12, className="text-center mt-4"))
+        ], className="border-top pt-3")
     ], fluid=True)
