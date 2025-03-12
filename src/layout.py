068d88551f9c4ca464fbcd739534eb0147b147de
Lukman Lateef <la325@student.ubc.ca>
2025-03-12 15:01:40 -0700

    adjusted alignment on cards

--- src/layout.py
+++ src/layout.py
@@ -182,7 +182,8 @@ def create_layout(df):
                 id="wordcloud",
                 figure=create_wordcloud(df, ["All"]),
                 config={"displayModeBar": False},
-                style={'width': '100%', 'height': '100%'}
+                style={'width': '100%', 
+                       'height': '100%'}
             )
         )
     ],
